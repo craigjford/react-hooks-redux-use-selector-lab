bd68e32de88c00ef84728c0f12ce604400330aec
Craig Ford <craig_ford@icloud.com>
2023-02-05 16:01:46 -0500

    commit tree c6a42e9516c21a678b85d577fd534c2280ce9bd7

--- src/features/users/Users.js
+++ src/features/users/Users.js
@@ -1,14 +1,19 @@
 import React from "react";
-// add any needed imports here
+import { useSelector } from "react-redux";
 
 function Users() {
+
+  const users = useSelector((state) => state.users);
+  const userNumber = useSelector((state) => state.users.length);
+   
+  console.log('users = ', users)
+
   return (
     <div>
       <ul>
-        Users!
-        {/* Write code here that displays the usernames of all users in the Redux store */}
-        {/* In addition, display the total number of users curently in the store */}
+        {users.map((user) => <li key={user.username}>Name: {user.username} - Hometown: {user.hometown}</li> )}
       </ul>
+      Total Users: {userNumber}
     </div>
   );
 }
